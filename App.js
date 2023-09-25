a44c86914270c5906b2f60aa421b070ec1b9f686
vikassinghrathor <rathorevikas2809@gmail.com>
2023-09-25 19:21:27 +0530

    Edit Feature

--- App.js
+++ App.js
@@ -2,76 +2,102 @@
 const studentForm = document.getElementById("studentForm");
 const studentsContainer = document.querySelector(".students");
 const nameInput = studentForm["name"];
-const ageInput = studentForm["email"];
-const rollInput = studentForm["Phone no"];
+const emailInput = studentForm["email"];
+const PhonenoInput = studentForm["Phone no"];
 
-/* 
-{
-  name: '',
-  age: number,
-  roll: number
-}
-*/
-
+// Load students from local storage or initialize an empty array
 const students = JSON.parse(localStorage.getItem("students")) || [];
 
+// Function to add a new student
 const addStudent = (name, email, Phoneno) => {
-  students.push({
+  const newStudent = {
     name,
     email,
     Phoneno,
-  });
+  };
 
+  students.push(newStudent);
   localStorage.setItem("students", JSON.stringify(students));
 
-  return { name, email, Phoneno };
+  return newStudent;
 };
 
-
-const createStudentElement = ({ name, email, Phoneno }, index) => {
+// Function to create a student element
+const createStudentElement = (student, index) => {
   // Create elements
   const studentDiv = document.createElement("div");
   const studentName = document.createElement("h2");
-  const studentemail = document.createElement("p");
+  const studentEmail = document.createElement("p");
   const studentPhoneno = document.createElement("p");
   const deleteButton = document.createElement("button");
+  const editButton = document.createElement("button");
 
   // Fill the content
-  studentName.innerText = "Student name: " + name;
-  studentemail.innerText = "Student email: " + email;
-  studentPhoneno.innerText = "Student Phone no: " + Phoneno;
+  studentName.innerText = "Student name: " + student.name;
+  studentEmail.innerText = "Student email: " + student.email;
+  studentPhoneno.innerText = "Student Phone no: " + student.Phoneno;
   deleteButton.innerText = "Delete";
+  editButton.innerText = "Edit";
 
+  // Add a click event listener to the Delete button
   deleteButton.addEventListener("click", () => {
     students.splice(index, 1);
-    
-    // Update local storage
     localStorage.setItem("students", JSON.stringify(students));
-    
     studentsContainer.removeChild(studentDiv);
   });
 
-  studentDiv.append(studentName, studentemail, studentPhoneno, deleteButton);
+  // Add a click event listener to the Edit button
+  editButton.addEventListener("click", () => {
+    // Load the student's details into the input fields
+    nameInput.value = student.name;
+    emailInput.value = student.email;
+    PhonenoInput.value = student.Phoneno;
+
+    // Handle form submission for editing
+    studentForm.onsubmit = (e) => {
+      e.preventDefault();
+
+      // Update the student's details in the students array
+      students[index] = {
+        name: nameInput.value,
+        email: emailInput.value,
+        Phoneno: PhonenoInput.value,
+      };
+
+      // Update local storage
+      localStorage.setItem("students", JSON.stringify(students));
+
+      // Clear the input fields
+      nameInput.value = "";
+      emailInput.value = "";
+      PhonenoInput.value = "";
+
+      // Remove the event listener to prevent further submissions
+      studentForm.onsubmit = (e) => {
+        e.preventDefault();
+      };
+    };
+  });
+
+  // Add elements to the studentDiv
+  studentDiv.append(studentName, studentEmail, studentPhoneno, deleteButton, editButton);
   studentsContainer.appendChild(studentDiv);
 
-  studentsContainer.style.display = students.length === 0 ? "none" : "flex";
+  studentsContainer.style.display = students.length === 0 ? "none" : "block";
 };
 
-studentsContainer.style.display = students.length === 0 ? "none" : "flex";
-
+// Display existing students on page load
+studentsContainer.style.display = students.length === 0 ? "none" : "block";
 students.forEach(createStudentElement);
 
-studentForm.onsubmit = e => {
+// Handle form submission for adding new students
+studentForm.onsubmit = (e) => {
   e.preventDefault();
 
-  const newStudent = addStudent(
-    nameInput.value,
-    ageInput.value,
-    rollInput.value
-  );
-
+  const newStudent = addStudent(nameInput.value, emailInput.value, PhonenoInput.value);
   createStudentElement(newStudent);
 
+  // Clear the input fields
   nameInput.value = "";
   emailInput.value = "";
   PhonenoInput.value = "";
